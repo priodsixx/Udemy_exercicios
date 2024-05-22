9363ce075b37d72a7eab80db6a9be5fb7cb4c7f4
Carlos Miguel Bezerra de Oliveira <priodhero@gmail.com>
2024-05-22 16:51:52 -0300

    Add files via upload

---
+++ Exercicio09/src/packs9/Main.java
@@ -0,0 +1,34 @@
+package packs9;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
+		
+		int duracao;
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		}
+		else {
+			duracao = 24 - horaInicial + horaFinal;
+		}
+		
+		System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+		
+		sc.close();
+		
+		}
+		
+		
+
+	}
+
+}
+}
